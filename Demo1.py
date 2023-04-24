b237c72e6e6d7ef2e58dd4804aeef062859c77b9
RoelJohnGuzman <123426505+RoelJohnGuzman@users.noreply.github.com>
2023-04-25 07:57:55 +0800

    Add files via upload

--- Demo1.py
+++ Demo1.py
@@ -1,12 +1,36 @@
 from tkinter import *
-window = Tk()
-window.geometry("500x400+10+10")
-window.title("My First Demo")
 
-btm1 = Button(window,text="Click me", fg="red",bg="yellow")
-btm1.place(x= 300,y=250)
-txtfld = Entry(window, border=2.50)
-txtfld.place(x=100,y=200)
-tat = Label(window,text="My First Demo", font= "Verdana")
-tat.place(x=180, y=100)
+window = Tk()
+window.geometry("500x400")
+
+label = Label(window,text="gender")
+label.place(relx=.5, rely=.5)
+label.pack(anchor= CENTER)
+
+var1= IntVar()
+var2= IntVar()
+
+
+r1= Radiobutton(window, text="Male",variable=var1)
+r2= Radiobutton(window, text="Female",variable=var2)
+r1.place(relx=.52, rely=.52)
+r2.place(relx=.55, rely=.55)
+r1.pack(anchor= CENTER)
+r2.pack(anchor= CENTER)
+
+check1 = Checkbutton(window,text="100-200")
+check2 = Checkbutton(window,text= "201-300")
+check3 = Checkbutton(window, text="301-400")
+check1.pack()
+check2.pack()
+check3.pack()
+
+label1 = Label(window, text="Select from list of fruit")
+label1.pack()
+
+list = Listbox(window, selectmode="Single")
+list.insert(1,"Apple")
+list.insert(2,"Orange")
+list.insert(3,"Mango")
+list.pack()
 window.mainloop()
